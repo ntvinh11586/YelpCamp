29918b793a08452fb8db5cf60fa5a72ff806503b
The Vinh Nguyen <ntvinh.11586@gmail.com>
2017-01-28 19:11:41 +0700

    Add drop table

--- app.js
+++ app.js
@@ -10,24 +10,26 @@ app.set("view engine", "ejs");
 ///// SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-// Campground.create(
-//   {
-//     name: "Salmon Creek",
-//     image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"
-//   }, function(err, campground) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("NEWLY CRATED CAMPGROUND :");
-//       console.log(campground);
-//     }
-//   }
-// );
+Campground.create(
+  {
+    name: "Salmon Creek",
+    image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
+    description: "Lorem inspum"
+  }, function(err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("NEWLY CRATED CAMPGROUND :");
+      console.log(campground);
+    }
+  }
+);
 
 var campgrounds = [
   {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
@@ -69,6 +71,10 @@ app.post("/campgrounds", function(req, res) {
 app.get("/campgrounds/new", function(req, res) {
   res.render("new");
   // res.render("landing");
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+  res.send
 });
 
 app.listen(3000, function() {
