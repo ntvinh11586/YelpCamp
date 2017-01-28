ddcc5043677e424734d11cd4465f1bc6c850034e
The Vinh Nguyen <ntvinh.11586@gmail.com>
2017-01-28 11:00:14 +0700

    Add campgrounds

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+  {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+  {name: "Granite Hill", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+  {name: "Moutain Goats Rest", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"}
+];
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-  var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name: "Granite Hill", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name: "Moutain Goats Rest", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"}
-  ];
   res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res) {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new");
+  // res.render("landing");
 });
 
 app.listen(3000, function() {
