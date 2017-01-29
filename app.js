8160998928a6bbfe5b7a151c5bc9a5feeecaf9d5
The Vinh Nguyen <ntvinh.11586@gmail.com>
2017-01-30 00:10:21 +0700

    Add comments

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get("/campgrounds", function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render("index", {campgrounds:allCampgrounds});
+      res.render("campgrounds/index", {campgrounds:allCampgrounds});
     }
   });
 });
@@ -40,7 +40,7 @@ app.post("/campgrounds", function(req, res) {
 });
 
 app.get("/campgrounds/new", function(req, res) {
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", function(req, res) {
@@ -48,8 +48,18 @@ app.get("/campgrounds/:id", function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render("show", {campground:foundCampground});
+      res.render("campgrounds/show", {campground:foundCampground});
     }
+  });
+});
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+      if (err) {
+        consloe.log(err);
+      } else {
+        res.render("comments/new", {campground: campground});
+      }
   });
 });
 
