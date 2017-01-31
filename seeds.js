c524f33d6641505bf494a742c36e23a76ff4e028
The Vinh Nguyen <ntvinh.11586@gmail.com>
2017-01-31 14:22:56 +0700

    Add routes and owner

--- seeds.js
+++ seeds.js
@@ -22,36 +22,36 @@ var data = [
 
 function seedDB() {
   Campground.remove({}, function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("removed campgrounds!");
-    }
+    // if (err) {
+    //   console.log(err);
+    // } else {
+    //   console.log("removed campgrounds!");
+    // }
 
-    data.forEach(function(seed) {
-      Campground.create(seed, function(err, campground) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("added a campground");
-
-            // create a comment
-            Comment.create(
-              {
-                text: "abc abc abc",
-                author: "Homer"
-              }, function(err, comment) {
-                if (err) {
-                  console.log(err);
-                } else {
-                  campground.comments.push(comment);
-                  campground.save();
-                  console.log("Created new comment");
-                }
-              });
-          }
-      });
-    });
+    // data.forEach(function(seed) {
+    //   Campground.create(seed, function(err, campground) {
+    //       if (err) {
+    //         console.log(err);
+    //       } else {
+    //         console.log("added a campground");
+    //
+    //         // create a comment
+    //         Comment.create(
+    //           {
+    //             text: "abc abc abc",
+    //             author: "Homer"
+    //           }, function(err, comment) {
+    //             if (err) {
+    //               console.log(err);
+    //             } else {
+    //               campground.comments.push(comment);
+    //               campground.save();
+    //               console.log("Created new comment");
+    //             }
+    //           });
+    //       }
+    //   });
+    // });
   });
 }
 
